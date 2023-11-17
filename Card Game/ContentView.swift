6e563d4ceea9870305020f3bff06c630e9de9c62
Alex <aschaffer8770@gmail.com>
2023-11-16 22:17:41 -0800

    Fully Functional War Card Game.

--- Card Game/ContentView.swift
+++ Card Game/ContentView.swift
@@ -8,15 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card13"
+    
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    
+    @State var cpuScore = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Image("background-cloth")
+            
+            VStack {
+                
+                Image("logo")
+                
+                HStack {
+                    Image(playerCard)
+                        .padding()
+                    Image(cpuCard)
+                        .padding()
+                }.padding()
+                
+                Button(action: {
+                    dealCards()
+                }, label: {
+                    Image("button")
+                })
+                
+                
+                HStack {
+                    VStack {
+                        Text("Player ").padding().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                        Text(String(playerScore)).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                    }
+                    VStack {
+                        Text("Bot").padding().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                        Text(String(cpuScore)).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                    }
+
+                }
+                
+                    
+            }
         }
+        .foregroundColor(.green)
         .padding()
     }
+    
+    func dealCards() {
+        //Randomizes players cards when deal button is pressed
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        //Randomize CPU cards
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+            
+        //Update scores and decide winner
+        if playerCardValue > cpuCardValue {
+            //adding one to player card
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            //adding 1 to cpu score
+            cpuScore += 1
+        }
+        else {
+            //tie results in a new deal
+            dealCards()
+        }
+    }
+    
 }
 
 #Preview {
